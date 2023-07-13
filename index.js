7a5181788d58734de7bdd109442bb3004fc0eac2
MiguelN <guelferreira15@gmail.com>
2023-07-13 12:46:17 +0100

    testes

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-while(true) {console.log("Boas");} 
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
+  });
+  ws.send("something");
+});
+
+//while(true) {console.log("Boas");} 
